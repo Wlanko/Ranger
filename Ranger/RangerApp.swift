eaac29b597f83212eb3df0c715fada1b3d4fd5ef
Vlad Kyrylenko <sorvigolovaqwerty@gmail.com>
2024-03-11 08:49:36 +0200

    Initial Commit

---
+++ Ranger/RangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RangerApp.swift
+//  Ranger
+//
+//  Created by Vlad Kyrylenko on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct RangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
